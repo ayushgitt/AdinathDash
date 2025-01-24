869a1e73eb24e2293167fc887ae84ad95efadd0c
Karan Bisht <karan161003@gmail.com>
2025-01-24 14:23:20 +0530

    update 3

--- server/server.js
+++ server/server.js
@@ -461,7 +461,7 @@ app.get("/lead-stats", async (req, res) => {
 })
 
 // Start server
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
